b459d699652bc587002b54c09070320fb236868b
davidecardelli <davide.cardelli.94@gmail.com>
2023-01-26 22:52:00 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Boolflix',
+  data() {
+    return {
+
+    }
+  },
+  components: {},
+  methods: {
+
+  }
+}
+</script>
+
+<template></template>
+
+<style lang="scss">
+
+</style>
