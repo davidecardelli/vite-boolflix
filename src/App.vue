79de0734e13a1a58cdf704301d41f16cd366c034
davidecardelli <davide.cardelli.94@gmail.com>
2023-01-27 17:58:38 +0100

    search and print films and series

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,7 @@ import SearchBar from './components/SearchBar.vue';
 export default {
   name: 'Boolflix',
   components: { SearchBar },
-  data() {
-    return { store, searchedTitle: '' }
-  },
+  data() { return { store, searchedTitle: '' } },
   computed: {
     urlConfig() {
       const { api_key, language } = api;
@@ -27,16 +25,46 @@ export default {
       this.searchedTitle = searchTerm;
     },
 
-    searchTerm() {
-      axios.get(`${api.baseUri}/search/movie`, this.urlConfig)
+    callApi(endpoint, typology) {
+      axios.get(`${api.baseUri}${endpoint}`, this.urlConfig)
         .then(res => {
-          this.store.films = res.data.results;
-        }).catch(err => { console.error(err) });
+          this.store[typology] = res.data.results;
+        })
     },
+
+    searchTerm() {
+      if (!this.searchedTitle) {
+        this.store.films = [];
+        this.store.series = [];
+        return;
+      }
+      this.callApi(api.getMovies, 'films')
+      this.callApi(api.getTvSeries, 'series')
+    },
+
   }
 }
 </script>
 
 <template>
   <search-bar @term-has-change="updateSearchTerm" @submit-form="searchTerm"></search-bar>
+
+  <h2>Film</h2>
+  <ul v-for="film in store.films" :key="film.id">
+    <li>{{ film.title }}</li>
+    <li>{{ film.original_title }}</li>
+    <li>{{ film.original_language }}</li>
+    <li>{{ film.vote_average }}</li>
+  </ul>
+
+  <h2>Serie</h2>
+  <ul v-for="serie in store.series" :key="serie.id">
+    <li>{{ serie.name }}</li>
+    <li>{{ serie.original_name }}</li>
+    <li>{{ serie.original_language }}</li>
+    <li>{{ serie.vote_average }}</li>
+  </ul>
+
+
+
 </template>
